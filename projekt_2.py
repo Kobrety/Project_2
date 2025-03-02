fa2527d5db61913da358044d10e16f261d242e99
Kobrety <kobracz.mk@gmail.com>
2025-03-02 10:26:49 +0100

    projekt_2.py

--- projekt_2.py
+++ projekt_2.py
@@ -1,61 +1,57 @@
 """
-projekt_2.py: druhý projekt do Engeto Online Python Akademie
+project_2.py: second project for Engeto Online Python Academy
 
 author: Michal Kolar
 email: kobracz.mkgmail.com
 """
 
-
-def vytvor_hraci_pole():
+def create_game_board():
     return [[" " for _ in range(3)] for _ in range(3)]
 
-def zobraz_hraci_pole(pole):
-    separator = "+---+---+---+"  # Tento řetězec bude oddělovat řádky
-    print(separator)  # Začátek hracího pole
-    for i, radek in enumerate(pole):
-        print("| " + " | ".join(radek) + " |")
+def display_game_board(board):
+    separator = "\t+---+---+---+"  
+    print(separator)
+    for i, row in enumerate(board):
+        print("\t| " + " | ".join(row) + " |")
         if i < 2:
-            print(separator)  # Oddělovač mezi řádky
-    print(separator)  # Konec hracího pole
+            print(separator)
+    print(separator)
 
-def je_vyhra(pole, hrac):
-    # Kontrola řádků, sloupců a diagonál
+def is_winner(board, player):
     for x in range(3):
-        if all(pole[x][y] == hrac for y in range(3)) or all(pole[y][x] == hrac for y in range(3)):
+        if all(board[x][y] == player for y in range(3)) or all(board[y][x] == player for y in range(3)):
             return True
-    if all(pole[x][x] == hrac for x in range(3)) or all(pole[x][2 - x] == hrac for x in range(3)):
+    if all(board[x][x] == player for x in range(3)) or all(board[x][2 - x] == player for x in range(3)):
         return True
     return False
 
-def je_remiza(pole):
-    return all(pole[x][y] != " " for x in range(3) for y in range(3))
+def is_draw(board):
+    return all(board[x][y] != " " for x in range(3) for y in range(3))
 
-def ziskej_tah(hrac, pole):
-    # Mapování čísel na pozice
-    pozice_mapovani = {
+def get_move(player, board):
+    position_mapping = {
         1: (0, 0), 2: (0, 1), 3: (0, 2),
         4: (1, 0), 5: (1, 1), 6: (1, 2),
         7: (2, 0), 8: (2, 1), 9: (2, 2),
     }
     
     while True:
-        print(f"========================================")
+        print("========================================")
         try:
-            tah = int(input(f"Hráč {hrac.lower()} | Zadej číslo pole (1-9): "))
-            if tah < 1 or tah > 9:
-                print("Neplatný vstup! Zadej číslo mezi 1 a 9.")
+            move = int(input(f"Player {player.lower()} | Enter field number (1-9): "))
+            if move < 1 or move > 9:
+                print("Invalid input! Enter a number between 1 and 9.")
                 continue
 
-            x, y = pozice_mapovani[tah]  # Získání pozice z mapování
-            if pole[x][y] != " ":
-                print("Toto pole je již obsazené, zkus jiné!")
+            x, y = position_mapping[move]
+            if board[x][y] != " ":
+                print("This field is already occupied, try another one!")
                 continue
             return x, y
         except ValueError:
-            print("Neplatný vstup! Zadej číslo mezi 1 a 9.")
+            print("Invalid input! Enter a number between 1 and 9.")
 
-def piskvorky():
-    # Zobrazení úvodních pravidel
+def tic_tac_toe():
     print("Welcome to Tic Tac Toe")
     print("========================================")
     print("GAME RULES:")
@@ -67,33 +63,32 @@ def piskvorky():
     print("========================================")
     print("Let's start the game\n")
     
-    # Zadání jmen hráčů
-    jmeno_1 = input("Zadejte jméno hráče X: ")
-    jmeno_2 = input("Zadejte jméno hráče O: ")
+    player_1 = input("Enter player X name: ")
+    player_2 = input("Enter player O name: ")
 
-    score = {jmeno_1: 0, jmeno_2: 0}
+    score = {player_1: 0, player_2: 0}
     while True:
-        pole = vytvor_hraci_pole()
-        hrac = jmeno_1  # Začíná hráč X
+        board = create_game_board()
+        player = player_1
         while True:
-            zobraz_hraci_pole(pole)
-            x, y = ziskej_tah(hrac, pole)
-            pole[x][y] = "X" if hrac == jmeno_1 else "O"
-            if je_vyhra(pole, "X" if hrac == jmeno_1 else "O"):
-                zobraz_hraci_pole(pole)
+            display_game_board(board)
+            x, y = get_move(player, board)
+            board[x][y] = "X" if player == player_1 else "O"
+            if is_winner(board, "X" if player == player_1 else "O"):
+                display_game_board(board)
                 print(f"============================================")
-                print(f"Congratulations, the player {hrac.lower()} WON!")
+                print(f"Congratulations, the player {player.lower()} WON!")
                 print(f"============================================")
-                score[hrac] += 1
+                score[player] += 1
                 break
-            if je_remiza(pole):
-                zobraz_hraci_pole(pole)
-                print("Remíza!")
+            if is_draw(board):
+                display_game_board(board)
+                print("It's a draw!")
                 break
-            hrac = jmeno_2 if hrac == jmeno_1 else jmeno_1
-        print(f"Skóre: {jmeno_1} - {score[jmeno_1]}, {jmeno_2} - {score[jmeno_2]}")
-        if input("Chcete hrát znovu? (ano/ne): ").lower() != "ano":
+            player = player_2 if player == player_1 else player_1
+        print(f"Score: {player_1} - {score[player_1]}, {player_2} - {score[player_2]}")
+        if input("Do you want to play again? Otherwise, the program will exit. (yes/no): ").lower() != "yes":
             break
 
 if __name__ == "__main__":
-    piskvorky()
+    tic_tac_toe()
